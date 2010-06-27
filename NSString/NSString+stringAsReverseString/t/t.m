1b44d572a0d83c449d128e0b9646a529a3c59ba7
nicerobot <git@nicerobot.org>
2010-06-27 17:16:40 -0400

    Added test-case project to main project.

--- NSString/NSString+stringAsReverseString/t/t.m
+++ NSString/NSString+stringAsReverseString/t/t.m
@@ -1,10 +1,11 @@
 #import <Foundation/Foundation.h>
+#import "NSString+stringAsReverseString/NSString+stringAsReverseString.h"
 
 int main (int argc, const char * argv[]) {
   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
   
   // insert code here...
-  NSLog(@"Hello, World!");
+  NSLog(@"%@", [@"Hello, World!" stringAsReverseString]);
   [pool drain];
   return 0;
 }
