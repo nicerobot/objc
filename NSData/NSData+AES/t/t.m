6adb272546e9c889160e5ddae31c11df103676a9
robert <robert@urban4m.com>
2014-03-08 21:43:58 -0500

    test

--- NSData/NSData+AES/t/t.m
+++ NSData/NSData+AES/t/t.m
@@ -24,28 +24,24 @@
 
 int main (int argc, const char * argv[]) {
   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-  /*
+
 	NSLog(@"testing symmetric encrypt/decrypt ...");
 	NSString *_key = @"hello";
 	NSString *_secret = @"hello";
 	NSData *_secretData = [_secret dataUsingEncoding:NSASCIIStringEncoding];
-	
+
 	NSLog(@"plaintext string: %@",[[NSString alloc] initWithData:_secretData encoding:NSASCIIStringEncoding]);
-	
-	CCOptions padding = kCCOptionPKCS7Padding;
-	NSData *encryptedData = [self encrypt:_secretData key:[self md5data:_key] padding:&padding];
+
+	NSData *encryptedData = [_secretData encryptWithString:_key];
 	
 	NSLog(@"encrypted data: %@", encryptedData);
 	
-	NSLog(@"encoded string ASCII: %@",[[NSString alloc] initWithData:encryptedData encoding:NSASCIIStringEncoding]);
-	NSLog(@"encoded string UTF8: %@",[[NSString alloc] initWithData:encryptedData encoding:NSUTF8StringEncoding]);
-	
-	NSData *data = [self decrypt:encryptedData key:[self md5data:_key] padding:&padding];
+	NSData *data = [encryptedData decryptWithString:_key];
 	NSLog(@"decrypted data: %@", data);
 	NSString *str = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
 	NSLog(@"decrypted string: %@", str);
 	NSLog(@"test finished.");
-  //*/
+
   [pool drain];
   return 0;
 }
